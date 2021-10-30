f129d6d80ecebf972f30ebc7abfe083203aef08c
Mahed21 <mahed255261@gmail.com>
2021-10-31 00:56:13 +0600

    third commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors=require('cors');
 const ObjectId=require('mongodb').ObjectId;
 require('dotenv').config() 
 const app = express()
-const port =5000
+const port =process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
@@ -40,9 +40,38 @@ async function run() {
         const newUser=req.body;
         const result = await userOrder.insertOne(newUser);
         res.json(result);
+     }) 
 
+      app.get('/order',async(req,res)=>
+      {
+        const cursor=userOrder.find({});
+        const products=await cursor.toArray();
+        res.send(products);
+      })
+      //add services from client 
+      app.post('/products',async (req,res)=>
+      {
+        const newUser=req.body;
+        //console.log(newUser);
+        const result=await userCollection.insertOne(newUser);
+        res.json(result);
 
       })
+      app.delete('/order/:id' , async (req,res)=>
+      {
+        const id =req.params.id;
+        const query ={_id : ObjectId(id)};
+        const result =await userOrder.deleteOne(query);
+        console.log('deletd');
+        res.json(result);
+        
+      })
+      //delete
+     
+      
+      
+
+      
       //console.log(`A document was inserted with the _id: ${result.insertedId}`);
     } finally {
       //await client.close();
